6ea958f29e49f9f2c7d47bb554b7d50016c7e5a3
senpaimarta <soymartalanzas@gmail.com>
2024-04-18 21:41:43 +0200

    final entrega

--- src/components/SearchComp.jsx
+++ src/components/SearchComp.jsx
@@ -1,34 +1,33 @@
-import { useEffect, useRef, useState } from 'react';
-import { getProdName } from '../firebase/firebase';
-import ItemBusq from '../components/ItemListContainer/ItemBusq'; // Asegúrate de que el componente está correctamente importado
+import { useEffect, useRef, useState } from 'react'
+import { getProdName } from '../firebase/firebase'
+import ItemBusq from '../components/ItemListContainer/ItemBusq'
 
 export default function SearchComp() {
-    const [prodPorBusq, setProdPorBusq] = useState([]);
-    const refCampo = useRef(null);
+    const [prodPorBusq, setProdPorBusq] = useState([])
+    const refCampo = useRef(null)
 
     const handleBusq = async () => {
-        const inputText = refCampo.current.value.trim();
-        console.log("Buscando:", inputText); // Mensaje de consola para confirmar la acción del botón
+        const inputText = refCampo.current.value.trim()
+        console.log("Buscando:", inputText)
         if (inputText) {
-            fetchData(inputText);
+            fetchData(inputText)
         } else {
-            console.error('Por favor, ingresa un término de búsqueda válido.');
+            console.error('Por favor, ingresa un término de búsqueda válido.')
         }
     };
 
     const fetchData = async (term) => {
         try {
-            const data = await getProdName(term);
-            setProdPorBusq(data);
-            console.log("Productos encontrados:", data); // Verificar los datos recibidos
+            const data = await getProdName(term)
+            setProdPorBusq(data)
+            console.log("Productos encontrados:", data)
         } catch (error) {
-            console.error('Error al buscar productos:', error);
+            console.error('Error al buscar productos:', error)
         }
     };
 
     useEffect(() => {
-        // Inicialmente enfocar el campo de búsqueda
-        refCampo.current.focus();
+        refCampo.current.focus()
     }, []);
 
     return (
